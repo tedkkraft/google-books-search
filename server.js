b593ed8391e904fb5791ebc6f476df9054818863
tedkkraft <tedkkraft@gmail.com>
2020-11-07 06:31:36 -0500

    Server added, Mongoose added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,6 +14,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Book", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
